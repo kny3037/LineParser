2aab30b8ed5ccf85841c1134cbc05250b7337733
psi5059 <psi505968@gmail.com>
2022-10-19 13:40:03 +0900

    Spring 적용

--- src/main/java/com/line/dao/UserDaoFactory.java
+++ src/main/java/com/line/dao/UserDaoFactory.java
@@ -1,7 +1,10 @@
 package com.line.dao;
 
+import org.springframework.context.annotation.Bean;
+
 public class UserDaoFactory {
     //조립을 해주는 역할
+    @Bean
     public UserDao awsUserDao(){  //날개 5개 선풍기
         AWSConnectionMaker awsConnectionMaker = new AWSConnectionMaker();
         UserDao userDao = new UserDao(awsConnectionMaker);
--- src/test/java/com/line/dao/UserDaoTest.java
+++ src/test/java/com/line/dao/UserDaoTest.java
@@ -3,18 +3,28 @@ package com.line.dao;
 import com.line.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
+
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
 
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
-       // UserDao userDao = new UserDao(new AWSConnectionMaker());
-        UserDao userDao = new UserDaoFactory().awsUserDao();
-        User user = new User("3","kim","1123");
+        UserDao userDao = context.getBean("awsUserDao",UserDao.class);
+        User user = new User("4","Lee","1123");
         userDao.add(user);
 
         User selectedUser = userDao.select("0");
