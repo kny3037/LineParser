96748ea4e3fdf685134f1c9de485e4ec3e5c12c2
psi5059 <psi505968@gmail.com>
2022-10-20 02:14:16 +0900

    어노테이션 설명 추가

--- src/test/java/com/line/dao/UserDaoTest.java
+++ src/test/java/com/line/dao/UserDaoTest.java
@@ -14,12 +14,25 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
+/*
+- 확장을 선언적으로 등록해 주는 역할을 한다.
+- Extension(확장자) 뒤에 인자로 확장할 Extension을 추가하여 사용 할 수 있다.
+- Spring을 사용할 경우 @ExtendWith(SpringExtension.class)와 같이 사용한다.
+- junit4 사용시에는 springRunner.class를 명시적으로 선언하고 사용해야 했으나
+- junit5들어와서는 @SpringBootTest 어노테이션안에 @ExtendsWith 명시가 되어있다.
+- 즉, junit5를 사용할 경우  @SpringBootTest 어노테이션을 사용하지 않았을 경우에만
+- @ExtendsWith를 직접적으로 명시를 해주면 되는 것이다.
+ */
 @ContextConfiguration(classes = UserDaoFactory.class)
-
+// Junit5 Test코드를 실행 할 때 ApplicationContext에 들어갈 설정 정보(관계 설정)를 불러오게 해주는 기능
+//
 class UserDaoTest {
 
     @Autowired
     ApplicationContext context;
+    //@Autowired
+    //의존성 주입을 할 때 사용하는 어노테이션으로 의존 객체의 타입에 해당하는 bean을 찾아 주입하는 역할
+
 
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
